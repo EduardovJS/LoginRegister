ca3aa6825413efe3bbf3897494c86fd3472a651d
EduardovJS <eduardoviniciusjorge@gmail.com>
2024-12-22 15:49:17 -0300

    get e post login

---
+++ LoginRegister/Controllers/AccountController.cs
@@ -0,0 +1,50 @@
+﻿using LoginRegister.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginRegister.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+
+        [HttpGet]
+        public IActionResult Login(string returnUtl = null)
+        {
+            return View(new AccountViewModel()
+            {
+                ReturnUrl = returnUtl
+            });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(AccountViewModel model, string returnUrl = null)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Name, model.Password, false,false);
+
+                if (result.Succeeded)
+                {
+                    // Se houver um returnUrl, redireciona o usuário para essa URL
+                    return Redirect(returnUrl);
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Falha no login.");
+                    return View(model);
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ LoginRegister/ViewModels/AccountViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace LoginRegister.ViewModels
+{
+    public class AccountViewModel
+    {
+
+        [Required(ErrorMessage = "Informe seu nome de usuário")]
+        [Display(Name= "Nome")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Informe o seu email de login")]
+        [Display(Name= "E-mail")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Informe sua senha de login")]
+        [Display(Name = "Senha")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+
+        public string ReturnUrl { get; set; }
+
+
+
+
+
+
+    }
+}
