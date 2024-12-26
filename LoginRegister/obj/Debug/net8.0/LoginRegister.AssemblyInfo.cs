b889c7a284ff45d74f5f8795e8f6f1dfe8a59982
EduardovJS <eduardoviniciusjorge@gmail.com>
2024-12-25 22:49:10 -0300

    partial view de login

--- LoginRegister/obj/Debug/net8.0/LoginRegister.AssemblyInfo.cs
+++ LoginRegister/obj/Debug/net8.0/LoginRegister.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoginRegister")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36a2fd6c2df46e92fb1d69a22a8c8824f6afd4f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de3568f660741eb0736628f9b1c72ee60add24d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoginRegister")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoginRegister")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
